c6b902aa4fa0cd1611435f876417fdfdc827b179
linusbjo <linusbjo@users.noreply.github.com>
2020-02-06 15:28:27 +0100

    Lagt till kontrollers, masterpage + WCF

---
+++ Busskort/Controllers/AnmalanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Busskort.Controllers
+{
+    public class AnmalanController : Controller
+    {
+        // GET: Anmalan
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
