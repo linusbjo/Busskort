5d360f1d6100514372c4105e6048ef7007a2410c
linusbjo <linusbjo@users.noreply.github.com>
2020-02-08 15:47:44 +0100

    Databaskoppling i service

---
+++ BusskortService/Anmälan.cs
@@ -0,0 +1,55 @@
+namespace BusskortService
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Anmälan
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ID { get; set; }
+
+        public int Årskurs { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Skola { get; set; }
+
+        public int barnPersonnummer { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Förnamn { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Efternamn { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string barnFörnamn { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string barnEfternamn { get; set; }
+
+        [Column("E-post")]
+        [Required]
+        [StringLength(50)]
+        public string E_post { get; set; }
+
+        public int Telefon { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Adress { get; set; }
+
+        public int Postnummer { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Ort { get; set; }
+    }
+}
